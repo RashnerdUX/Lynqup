9ad19690b4d77dd17c05c321c935612bbc159ecd
Rashnerd <akhigbek6@gmail.com>
2025-07-14 11:07:24 +0100

    fix: Update URL patterns for mentorship app and improve documentation routes

--- backend/lynqup/urls.py
+++ backend/lynqup/urls.py
@@ -24,9 +24,13 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #The apps
     path('api/v1/users/', include('users.urls')),
+    path('api/v1/mentor/', include('mentorship.urls')),
+    #JWT Authentication
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    #Documentation
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v1/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/v1/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
