b9bb199f44633ae2affd783b515640637bdd8dce
Rashnerd <akhigbek6@gmail.com>
2025-07-21 22:42:52 +0100

    fix: Added Redis host and port to allowed hosts

--- backend/lynqup/settings.py
+++ backend/lynqup/settings.py
@@ -72,6 +72,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1",
+    "http://127.0.0.1:6379",
     "null",
     #In production, this will refer to our main domain
 ]
